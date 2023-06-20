5f7325d94f5b9c154cffc964a1eecefbd05eb7ac
Ермаков Денис <DEErmakov@lanit.ru>
2023-06-20 12:02:00 +0300

    Добавлено логирование выполненных задач

--- src/main/java/benchmark/app/impl/work/ServiceTaskUseCase.java
+++ src/main/java/benchmark/app/impl/work/ServiceTaskUseCase.java
@@ -3,12 +3,14 @@ package benchmark.app.impl.work;
 import benchmark.app.api.ServiceTaskInbound;
 import benchmark.app.impl.StatisticsCollector;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ServiceTaskUseCase implements ServiceTaskInbound {
 
     private final StatisticsCollector statisticsCollector;
@@ -17,5 +19,10 @@ public class ServiceTaskUseCase implements ServiceTaskInbound {
     public void execute(Map<String, Object> variables) {
         variables.put("key1", "value1");
         statisticsCollector.incCompletedJobs();
+
+        long cnt = statisticsCollector.getCompletedJobsMeter().getCount();
+        if (cnt % 100 == 0) {
+            log.info("Completed {} jobs", cnt);
+        }
     }
 }
